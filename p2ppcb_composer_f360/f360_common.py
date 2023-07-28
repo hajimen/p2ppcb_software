57cd3507ac914934aea8a6dba38c6b09abe11894
NAKAZATO Hajime <hajime@kaoriha.org>
2023-07-28 20:01:48 +0900

    ok

--- p2ppcb_composer_f360/f360_common.py
+++ p2ppcb_composer_f360/f360_common.py
@@ -9,12 +9,21 @@ import pathlib
 import traceback
 import typing as ty
 from contextlib import contextmanager
-from pint import Quantity
 import p2ppcb_parts_resolver.resolver as parts_resolver
 from p2ppcb_parts_resolver.resolver import SpecsOpsOnPn
 import f360_insert_decal_rpa
 import adsk.core as ac
 import adsk.fusion as af
+
+
+import struct
+if struct.unpack('<d', b'\1\0\0\0\0\0\0\0')[0] == 0.0:
+    # bug workaround for Intel Mac F360.
+    try:
+        import numpy  # noqa
+    except RuntimeError:
+        pass
+from pint import Quantity
 
 FLOOR_CLEARANCE = 0.1
 
